38f332dc839ddd8f9a9de034091b5a00b1d39b56
wangliang <wangliang199538@live.com>
2022-09-15 21:07:56 +0800

    初始化循环链表

---
+++ circular_linked_list/circular_linked_list.c
@@ -0,0 +1,31 @@
+//
+// Created by 亮王 on 2022/9/15.
+//
+
+#include "circular_linked_list.h"
+
+
+LinkList InitList(){
+    LinkList l = (LNode*) malloc(sizeof(LNode));
+    if (l == NULL) return NULL;
+    l->next = l;
+    return l;
+}
+
+bool Empty(LinkList l){
+    if (l->next == l) return true;
+    return false;
+}
+
+int main(){
+
+    LinkList  l = InitList();
+
+    if (Empty(l)){
+        printf("is empty\n");
+    } else{
+        printf("not empty\n");
+    }
+
+    return 0;
+}
---
+++ circular_linked_list/circular_linked_list.h
@@ -0,0 +1,22 @@
+//
+// Created by 亮王 on 2022/9/15.
+//
+
+#ifndef DLINKED_CIRCULAR_LINKED_LIST_H
+#define DLINKED_CIRCULAR_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct LNode{
+    int data;
+    struct LNode* next;
+} LNode, *LinkList;
+
+
+LinkList InitList();
+
+bool Empty(LinkList l);
+
+#endif
